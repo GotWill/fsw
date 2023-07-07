aba75ff095cd8901f3bd110dbae05a17aa1122fa
Willian Pereira <williansouza9687a@gmail.com>
2023-07-07 17:56:57 -0300

    fex: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
-import Image from 'next/image'
-import { prisma } from './lib/prisma'
-
-const tripes = async () => {
-  const allTrips = prisma.trip.findMany({})
-
-  return allTrips
-}
-
-export  default async function Home() {
-
-  const data = await tripes();
-
-  console.log({data})
-
-
+export default function Home() {
 
   return (
-  <main>
-
-  </main>
+    <main>
+      
+    </main>
   )
 }
