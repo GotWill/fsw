dedc72798107dc0a6ffdb8b0201f4e35d278c3c5
Willian Pereira <williansouza9687a@gmail.com>
2023-07-08 18:11:00 -0300

    feat: add initial search trip

--- src/components/TripSearch.tsx
+++ src/components/TripSearch.tsx
@@ -1,22 +1,30 @@
 'use client'
 
+import Button from "./Button";
 import CurrencyInput from "./CurrencyInput";
 import DatePicker from "./DatePicker";
 import Input from "./Input";
 
-export function TripSearch(){
+export function TripSearch() {
     return (
-        <div className="container mx-auto p-5">
+        <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
             <h1 className="font-semibold text-2xl text-primaryDarker text-center">Encontre a sua próxima <span className="text-primary">estadia!</span></h1>
 
             <div className="flex flex-col gap-4 mt-5">
-                <Input placeholder="Onde voce quer ir?"/>
+                <Input placeholder="Onde voce quer ir?" />
 
                 <div className="flex gap-4">
-                  <DatePicker placeholderText="Data de ida" className="w-full" onChange={() => {}}/> 
-                  <CurrencyInput placeholder="Orçamento"/> 
+                    <DatePicker placeholderText="Data de ida" className="w-full" onChange={() => { }} />
+                    <CurrencyInput placeholder="Orçamento" />
+
                 </div>
+
+                <Button>
+                    Buscar
+                </Button>
             </div>
+
+
         </div>
     )
 }
