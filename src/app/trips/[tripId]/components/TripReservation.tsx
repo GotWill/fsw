8b3b4dafd3904e6d156cba8f72ee29dfeccf3919
Willian Pereira <williansouza9687a@gmail.com>
2023-07-09 20:47:29 -0300

    feat: add interface to trip confirmation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -4,6 +4,7 @@ import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
 import { Trip } from "@prisma/client";
 import { differenceInDays } from "date-fns";
+import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
 
 interface TripReservationProps {
@@ -26,6 +27,8 @@ interface TripReservationForm {
 export function TripReservation({tripId, maxGuests, pricePerDay, tripEndDate, tripStartDate }: TripReservationProps) {
 
     const { register, handleSubmit, formState: {errors}, control, watch, setError } = useForm<TripReservationForm>()
+
+    const router = useRouter();
 
     const onSubmit = async (data: TripReservationForm) => {
         const response = await fetch('http://localhost:3000/api/trips/check', {
@@ -71,6 +74,9 @@ export function TripReservation({tripId, maxGuests, pricePerDay, tripEndDate, tr
         }
 
 
+        router.push(
+            `/trips/${tripId}/confirmation?startDate=${data.startDate?.toISOString()}&endDate=${data.endDate?.toISOString()}&guests=${data.guests}`
+        )
         
     }
 
