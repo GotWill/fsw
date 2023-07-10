f6cbb9560cbeb766ce407aa1aa5116285269ac81
Willian Pereira <williansouza9687a@gmail.com>
2023-07-09 21:03:25 -0300

    feat: ensure user is redirect as error in trip confirmation

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -18,7 +18,7 @@ function TripConfirmation({ params }: { params: { tripId: string } }) {
 
     const searchParams = useSearchParams()
 
-    const {status} = useSession();
+    const { status } = useSession();
 
     const router = useRouter()
 
@@ -33,76 +33,83 @@ function TripConfirmation({ params }: { params: { tripId: string } }) {
 
                 })
             })
-            const { trip, totalPrice } = await response.json();
-            setTrip(trip)
-            setTotalPrice(totalPrice)
+
+
+            const req = await response.json()
+
+            if(req?.error){
+              return router.push('/')
+            }
+
+            setTrip(req.trip)
+            setTotalPrice(req.totalPrice)
 
         }
 
-        if(status === 'unauthenticated'){
+        if (status === 'unauthenticated') {
             router.push('/')
         }
 
         fetchTrip()
-    },[status])
+    }, [status, params.tripId,searchParams, router])
 
     if (!trip) return null
 
     const startDate = new Date(searchParams.get('startDate') as string)
     const endDate = new Date(searchParams.get('endDate') as string)
-    const guests = searchParams.get('guests') 
+    const guests = searchParams.get('guests')
 
 
 
 
     return (
         <div className="container mx-auto p-5 lg:max-w-[600px]">
-        <h1 className="font-semibold text-xl text-primaryDarker">Sua viagem</h1>
-  
-        {/* CARD */}
-        <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
-          <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
-            <div className="relative h-[106px] w-[124px]">
-              <Image src={trip.coverImage} fill style={{ objectFit: "cover" }} className="rounded-lg" alt={trip.name} />
+            <h1 className="font-semibold text-xl text-primaryDarker">Sua viagem</h1>
+
+            {/* CARD */}
+            <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
+                <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
+                    <div className="relative h-[106px] w-[124px]">
+                        <Image src={trip.coverImage} fill style={{ objectFit: "cover" }} className="rounded-lg" alt={trip.name} />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
+                        <div className="flex items-center gap-1">
+                            <ReactCountryFlag countryCode={trip.countryCode} svg />
+                            <p className="text-xs text-grayPrimary underline">{trip.location}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <h3 className="font-semibold text-lg text-primaryDarker mt-3">Informações sobre o preço</h3>
+
+                <div className="flex justify-between mt-1">
+                    <p className="text-primaryDarker">Total:</p>
+                    <p className="font-medium">R$ {totalPrice}</p>
+                </div>
             </div>
-  
-            <div className="flex flex-col">
-              <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
-              <div className="flex items-center gap-1">
-                <ReactCountryFlag countryCode={trip.countryCode} svg />
-                <p className="text-xs text-grayPrimary underline">{trip.location}</p>
-              </div>
+
+            <div className="flex flex-col mt-5 text-primaryDarker">
+                <h3 className="font-semibold">Data</h3>
+                <div className="flex items-center gap-1 mt-1">
+                    <p>
+                        {format(startDate, "dd 'de' MMMM", { locale: ptBr })}
+                    </p>
+                    {" - "}
+                    <p>
+                        {format(endDate, "dd 'de' MMMM", { locale: ptBr })}
+                    </p>
+                </div>
+
+                <h3 className="font-semibold mt-5">Hóspedes</h3>
+                <p> hóspedes {guests} </p>
+
+                <Button className="mt-5">
+                    Finalizar Compra
+                </Button>
             </div>
-          </div>
-  
-          <h3 className="font-semibold text-lg text-primaryDarker mt-3">Informações sobre o preço</h3>
-  
-          <div className="flex justify-between mt-1">
-            <p className="text-primaryDarker">Total:</p>
-            <p className="font-medium">R$ {totalPrice}</p>
-          </div>
         </div>
-  
-        <div className="flex flex-col mt-5 text-primaryDarker">
-          <h3 className="font-semibold">Data</h3>
-          <div className="flex items-center gap-1 mt-1">
-            <p>
-                {format(startDate, "dd 'de' MMMM", {locale: ptBr})}
-            </p>
-            {" - "}
-            <p>
-            {format(endDate, "dd 'de' MMMM", {locale: ptBr})}
-            </p>
-          </div>
-  
-          <h3 className="font-semibold mt-5">Hóspedes</h3>
-          <p> hóspedes {guests} </p>
-   
-          <Button className="mt-5">
-            Finalizar Compra
-          </Button>
-        </div>
-      </div>
     )
 }
 
