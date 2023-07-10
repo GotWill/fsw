3de1d266cd4e5bc88e7b0e2356767c69c3f8b376
Willian Pereira <williansouza9687a@gmail.com>
2023-07-09 23:56:05 -0300

    feat: add user reservations page

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -5,34 +5,41 @@ import { authOptions } from "../api/auth/[...nextauth]/route"
 import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
-import { TripReservation } from "@prisma/client"
+import { Prisma, TripReservation } from "@prisma/client"
+import { UserReservationItem } from './components/userReservationItem'
 
- function MyTrips (){
+function MyTrips() {
 
-    const {status, data} = useSession()
-    const [reservations, setReservations] = useState<TripReservation[]>([])
+    const { status, data } = useSession()
+    const [reservations, setReservations] = useState<
+        Prisma.TripReservationGetPayload<{
+            include: { trip: true };
+        }>[]
+    >([]);
 
     const router = useRouter()
 
 
 
     useEffect(() => {
-        if(status === 'unauthenticated' || !data?.user){
-          return  router.push('/')
+        if (status === 'unauthenticated' || !data?.user) {
+            return router.push('/')
         }
 
-      async function fetchReservations(){
-        const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/reservations`)
-        const json = await response.json()
-        setReservations(json)
-      }
+        async function fetchReservations() {
+            const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/reservations`)
+            const json = await response.json()
+            setReservations(json)
+        }
 
-      fetchReservations()
-    },[status])
-   
+        fetchReservations()
+    }, [status])
+
     return (
-        <div>
-            
+        <div className="container mx-auto p-5">
+            <h1 className="font-semibold text-xl text-primaryDarker">Minhas viagens</h1>
+
+            {reservations.map(reservation => <UserReservationItem key={reservation.id} reservation={reservation} />)}
         </div>
     )
 }
