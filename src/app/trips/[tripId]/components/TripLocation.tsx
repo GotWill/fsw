8e54d826854cd4a44de6689276f50efed14d33ab
Willian Pereira <williansouza9687a@gmail.com>
2023-07-12 14:12:48 -0300

    style: adapt trip details to desktop

--- src/app/trips/[tripId]/components/TripLocation.tsx
+++ src/app/trips/[tripId]/components/TripLocation.tsx
@@ -8,19 +8,29 @@ interface TripLocationProps {
 
 export function TripLocation({ location, locationDescription }: TripLocationProps) {
     return (
-        <div className="p-5">
-            <h2 className="font-semibold text-primaryDarker mb-5">Localização</h2>
-            <div className="relative h-[250px] w-full">
+        <div className="p-5 lg:p-0 lg:mt-12 lg:pb-20">
+            <h2 className="font-semibold text-primaryDarker mb-5 lg:text-xl">Localização</h2>
+            <div className="relative h-[250px] w-full lg:hidden">
                 <Image src="/map-mobile.png" fill style={{
                     objectFit: 'cover'
                 }} alt={location} 
-                className="rounded-lg shadow-md"
+                className="rounded-lg shadow-md "
                 />
 
             </div>
 
-            <h3 className="text-primaryDarker text-sm font-semibold mt-3">{location}</h3>
-            <p className="text-xs text-primaryDarker mt-2 leading-5">{locationDescription}</p>
+
+            <div className="relative h-[480px] w-full hiddem lg:block">
+                <Image src="/map-desktop.png" fill style={{
+                    objectFit: 'cover'
+                }} alt={location} 
+                className="rounded-lg shadow-md "
+                />
+
+            </div>
+
+            <h3 className="text-primaryDarker text-sm font-semibold mt-3 lg:text-base lg:mt-5">{location}</h3>
+            <p className="text-xs text-primaryDarker mt-2 leading-5 lg:text-base lg:mt-4">{locationDescription}</p>
             <Button variant="outlined" className="w-full mt-5">Ver no Google Maps</Button>
         </div>
     )
