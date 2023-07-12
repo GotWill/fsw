27c31d43d18fd8cc2b14935d99b36452c5013090
Willian Pereira <williansouza9687a@gmail.com>
2023-07-12 13:36:00 -0300

    adapt search trip to desjtop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -35,12 +35,12 @@ export default  function Trips(){
 
 
     return (
-        <div className="container mx-auto flex flex-col p-5 justify-center items-center">
-            <h1 className="text-primaryDarker font-semibold text-xl">Viagens encontradas</h1>
-            <h2 className="text-grayPrimary font-medium mb-5">{trips.length > 0 ? 'Listamos as melhores viagens para você' : 'Não encontramos nenhuma viagen!'}</h2>
+        <div className="container mx-auto flex flex-col p-5 justify-center items-center lg:items-start">
+            <h1 className="text-primaryDarker font-semibold text-xl lg:text-[2.5rem] lg:w-full lg:text-left">Viagens encontradas</h1>
+            <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">{trips.length > 0 ? 'Listamos as melhores viagens para você' : 'Não encontramos nenhuma viagen!'}</h2>
           
 
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 lg:flex-row lg:flex-wrap  lg:gap-10 lg:mt-6 lg:pb-16">
            {trips.map((trip) => <TripItem trip={trip} key={trip.id}/>)}
           </div>
         </div>
