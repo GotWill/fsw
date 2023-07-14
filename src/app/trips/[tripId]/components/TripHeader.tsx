a6299570cb440774823f7d1c7bc3aee028cdc5af
Willian Pereira <williansouza9687a@gmail.com>
2023-07-14 17:25:22 -0300

    style: add slider images mobol

--- src/app/trips/[tripId]/components/TripHeader.tsx
+++ src/app/trips/[tripId]/components/TripHeader.tsx
@@ -1,22 +1,86 @@
+'use client'
+
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 import { Trip } from "@prisma/client";
 import Image from "next/image";
 import ReactCountryFlag from "react-country-flag";
+import { useState } from "react";
+
+
 
 
 interface HeaderProps {
-    trip: Trip
+  trip: Trip
 }
 
-export function TripHeader({trip}: HeaderProps) {
-    return (
-        <div className="flex flex-col">
-             <div className="relative h-[300px] w-full lg:hidden">
-                <Image src={trip.coverImage} fill alt={trip.name} style={{
-                    objectFit: 'cover'
-                }} />
-            </div>
+export function TripHeader({ trip }: HeaderProps) {
 
-            <div className="hidden lg:grid grid-cols-[2fr,1fr,1fr] gap-2 grid-rows-2 lg:order-2">
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
+
+
+
+  return (
+    <div className="flex flex-col">
+      <div className="relative lg:hidden">
+        <div ref={sliderRef} className="keen-slider">
+
+          {
+            trip.imagesUrl.map((img) => {
+              return (
+                <div key={img} className="keen-slider__slide   flex items-center justify-center text-[50px] text-white font-medium h-[300px] max-h-[100vh]">
+                  <div className="relative h-[300px] w-full">
+                    <Image src={img} fill alt={trip.name} style={{
+                      objectFit: 'cover'
+                    }} />
+                  </div>
+                </div>
+              )
+            })
+          }
+
+
+        </div>
+        {loaded && instanceRef.current && (
+          <>
+            <Arrow
+              left
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+              disabled={currentSlide === 0}
+            />
+
+            <Arrow
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
+              }
+            />
+          </>
+        )}
+      </div>
+      
+
+
+
+
+
+
+      <div className="hidden lg:grid grid-cols-[2fr,1fr,1fr] gap-2 grid-rows-2 lg:order-2">
         <div className="relative row-span-2">
           <Image
             src={trip.coverImage}
@@ -78,19 +142,47 @@ export function TripHeader({trip}: HeaderProps) {
         </div>
       </div>
 
-            <div className="flex flex-col p-5 lg:p-0 lg:mb-10 lg:pt-10">
-                <h1 className="font-semibold text-xl text-primaryDarker lg:text-3xl">{trip.name}</h1>
-                <div className="flex items-center gap-1 my-1">
-                    <ReactCountryFlag countryCode={trip.countryCode} svg />
-                    <p className="text-xs text-grayPrimary underline lg:text-base">{trip.location}</p>
-                </div>
-
-                <p className="text-xs text-grayPrimary lg:hidden">
-                    <span className="text-primary font-medium">R$ {trip.pricePerDay.toString()} </span>
-                    por dia
-                </p>
-
-            </div>
+      <div className="flex flex-col p-5 lg:p-0 lg:mb-10 lg:pt-10">
+        <h1 className="font-semibold text-xl text-primaryDarker lg:text-3xl">{trip.name}</h1>
+        <div className="flex items-center gap-1 my-1">
+          <ReactCountryFlag countryCode={trip.countryCode} svg />
+          <p className="text-xs text-grayPrimary underline lg:text-base">{trip.location}</p>
         </div>
-    )
+
+        <p className="text-xs text-grayPrimary lg:hidden">
+          <span className="text-primary font-medium">R$ {trip.pricePerDay.toString()} </span>
+          por dia
+        </p>
+
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+function Arrow(props: {
+  disabled: boolean
+  left?: boolean
+  onClick: (e: any) => void
+}) {
+  const disabeld = props.disabled ? " fill-[#ffffff80]" : ""
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow w-[30px] h-[30px] translate-y-[-50%]  absolute top-2/4 fill-white cursor-pointer ${props.left ? "left-1" : "left-auto right-1"
+        } ${disabeld}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  )
 }
--- src/app/trips/[tripId]/components/TripLocation.tsx
+++ src/app/trips/[tripId]/components/TripLocation.tsx
@@ -1,37 +1,32 @@
 import Button from "@/components/Button";
-import Image from "next/image";
+import Link from "next/link";
 
 interface TripLocationProps {
     location: string;
     locationDescription: string;
 }
 
+
 export function TripLocation({ location, locationDescription }: TripLocationProps) {
     return (
         <div className="p-5 lg:p-0 lg:mt-12 lg:pb-20">
             <h2 className="font-semibold text-primaryDarker mb-5 lg:text-xl">Localização</h2>
-            <div className="relative h-[250px] w-full lg:hidden">
-                <Image src="/map-mobile.png" fill style={{
-                    objectFit: 'cover'
-                }} alt={location} 
-                className="rounded-lg shadow-md "
-                />
 
-            </div>
+            <iframe
+               className="w-full h-[450px]"
+                loading="lazy"
+                src={`https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAPS_KEY}&q=${encodeURI(location)}`}>
+            </iframe>
 
 
-            <div className="relative h-[480px] w-full hidden lg:block">
-                <Image src="/map-desktop.png" fill style={{
-                    objectFit: 'cover'
-                }} alt={location} 
-                className="rounded-lg shadow-md "
-                />
-
-            </div>
 
             <h3 className="text-primaryDarker text-sm font-semibold mt-3 lg:text-base lg:mt-5">{location}</h3>
             <p className="text-xs text-primaryDarker mt-2 leading-5 lg:text-base lg:mt-4">{locationDescription}</p>
-            <Button variant="outlined" className="w-full mt-5">Ver no Google Maps</Button>
+            <Button variant="outlined" className="w-full mt-5">
+               <Link target="_blank" href={`https://www.google.com/maps/search/?api=${process.env.GOOGLE_MAPS_KEY}&q=${encodeURI(location)}`}>
+                 Ver no Google Maps
+               </Link>
+            </Button>
         </div>
     )
 }
